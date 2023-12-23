97450c061b8796a1172f4776b86190e34cfe6b6d
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-12-23 21:36:11 +0200

    refactor agglo_knife

--- agglomerative_cluster.py
+++ agglomerative_cluster.py
@@ -1,10 +1,11 @@
 import numpy as np
 import tqdm
 from sklearn.cluster import AgglomerativeClustering
-from sklearn.metrics import silhouette_score
+from sklearn.metrics import silhouette_score, silhouette_samples
 import pandas as pd
 
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import seaborn as sns
 
 
@@ -81,10 +82,11 @@ class AgglomerativeCluster:
 
     def agglo_knife(
             self,
-            min_k,
-            max_k,
+            min_k: int,
+            max_k: int,
             step=1,
-            knife=True
+            knife: bool = True,
+
     ):
         # DESCRIPTION:
 
@@ -95,5 +97,73 @@ class AgglomerativeCluster:
 
         #     min_k - minimum amount of cluster
         #     max_k - maximum amount of cluster
-        #     step - step between clusters
-        #     knife - if True, knife metric will be represented
+
+        range_n_clusters = list(range(min_k, max_k, step))
+
+        n_clus = []
+        silhouette = []
+
+        for n_clusters in tqdm(
+                range_n_clusters,
+                desc="Checking knifes"
+        ):
+            fig, ax1 = plt.subplots(1, 1)
+            fig.set_size_inches(7, 4)
+
+            cluster = AgglomerativeClustering(n_clusters=n_clusters)
+            cluster_labels = cluster.fit_predict(self.X)
+
+            n_clus.append(n_clusters)
+            silhouette.append(round(
+                silhouette_score(self.X, cluster_labels), 3)
+            )
+
+            print(
+                "n_clusters =",
+                n_clusters,
+                "average silhouette_score =",
+                round(silhouette_score(self.X, cluster_labels), 3))
+
+            if knife:
+
+                sample_silhouette_values = silhouette_samples(
+                    self.X,
+                    cluster_labels
+                )
+
+                y_lower = 10
+                for i in range(n_clusters):
+                    ith_cluster_silhouette_values = (
+                        sample_silhouette_values[cluster_labels == i]
+                    )
+
+                    ith_cluster_silhouette_values.sort()
+
+                    size_cluster_i = ith_cluster_silhouette_values.shape[0]
+                    y_upper = y_lower + size_cluster_i
+                    color = cm.nipy_spectral(float(i) / n_clusters)
+                    ax1.fill_betweenx(
+                        np.arange(y_lower, y_upper),
+                        0,
+                        ith_cluster_silhouette_values,
+                        facecolor=color,
+                        edgecolor=color,
+                        alpha=0.7,)
+
+                    ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+                    y_lower = y_upper + 10
+
+                ax1.set_title("Silhouette plot for n_clusters = %d" % n_clusters)
+                ax1.set_xlabel("Silhouette coefficient values")
+                ax1.set_ylabel("Cluster label")
+                ax1.axvline(x=silhouette_score(self.X, cluster_labels), color="red", linestyle="--")
+                ax1.set_yticks([])
+                ax1.set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
+
+                plt.show()
+
+        df = pd.DataFrame({
+            'silhouette': silhouette},
+            index=range(min_k, max_k, step))
+
+        return df.transpose()
