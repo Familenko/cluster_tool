51471683b8b2758d1f738909c299705463755165
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-12-23 21:12:50 +0200

    refactor agglo_distance

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ import seaborn as sns
 
 class Clusterer:
     def __init__(self, X: pd.DataFrame):
-        self.df = X
-        self.X = X
+        self.df: pd.DataFrame = X
+        self.X: pd.DataFrame = X
 
-    def agglo_distance(self, min_d=1, max_d=5, range_d=10, save=None):
+    def agglo_distance(self, min_d=1, max_d=5, range_d=10):
         # DESCRIPTION:
 
         #     Build diagram with distance correlation of data in different cluster
@@ -26,7 +26,7 @@ class Clusterer:
 
         distance_threshold_list = list(np.linspace(min_d, max_d, range_d))
 
-        n_claster_dis = []
+        n_cluster_dis = []
         distance_threshold = []
         silhouette_dis = []
 
@@ -34,40 +34,43 @@ class Clusterer:
                 distance_threshold_list,
                 desc="Checking distance"
         ):
-            clusterer = AgglomerativeClustering(
+
+            cluster = AgglomerativeClustering(
                 n_clusters=None,
                 distance_threshold=distance_threshold_n
             )
-            cluster_labels = clusterer.fit_predict(self.X)
+            cluster_labels = cluster.fit_predict(self.X)
 
-            distance_threshold.append(distance_threshold_n)
-            n_claster_dis.append(len(np.unique(cluster_labels)))
+            distance_threshold.append(
+                distance_threshold_n
+            )
+            n_cluster_dis.append(
+                len(np.unique(cluster_labels))
+            )
             silhouette_dis.append(
                 round(silhouette_score(self.X, cluster_labels), 3),
             )
 
         plt.figure(figsize=(12, 5), dpi=200)
         sns.scatterplot(
-            x=n_claster_dis,
+            x=n_cluster_dis,
             y=distance_threshold,
             hue=silhouette_dis,
             s=100
         )
+
         plt.title("Distance plot")
-        plt.xlabel("Number of claster")
+        plt.xlabel("Number of cluster")
         plt.ylabel("Distance_threshold")
-        plt.xticks(range(min(n_claster_dis), max(n_claster_dis) + 1))
+        plt.xticks(range(min(n_cluster_dis), max(n_cluster_dis) + 1))
         plt.yticks(np.arange(0, max(distance_threshold) + 1, step=1))
         plt.legend(title="Silhouette", loc="upper right")
-
-        if save == "save":
-            plt.savefig("my_plot.png")
 
         plt.show()
 
         df = pd.DataFrame(
             {
-                "n_clusters": n_claster_dis,
+                "n_clusters": n_cluster_dis,
                 "distance_threshold": distance_threshold,
                 "silhouette": silhouette_dis,
             }
